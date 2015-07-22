06af6f539072bef2ae02f3d91e42a968864688fc
stephtdouglas <stephanietdouglas@gmail.com>
2015-07-22 10:18:49 -0400

    mostly stylistic changes

--- line_tools.py
+++ line_tools.py
@@ -25,41 +25,64 @@ Returns:
 
 def equivalent_width(filename,xmin,xmax,exclude_min,exclude_max,n):
     sp = p.Spectrum(filename)
-    sp.xarr.units ='microns'                                                           
+    sp.xarr.units = 'micron' 
     sp.xarr.xtype = 'wavelength'
-    sp.plotter(xmin=xmin, xmax=xmax, ymin=0, errstyle='bars', color='grey')                                         
-    sp.baseline(xmin=xmin, xmax=xmax, exclude=[exclude_min,exclude_max], 
+    print sp.xarr
+    print sp.data
+
+    sp.plotter(xmin=xmin, xmax=xmax, ymin=0, errstyle='bars', 
+               color='grey')
+    sp.baseline(xmin=xmin, xmax=xmax,  
+                exclude=[exclude_min,exclude_max], 
                 subtract=False, highlight_fitregion=False, 
                 selectregion=True, order=0)    
     sp.specfit(plot=True, fittype='voigt', color='blue',
                guesses='moments', vheight=True)
-    sp.specfit.EQW(plot=True, plotcolor='g', fitted=False, components=False, 
-                   annotate=True, loc='lower left', xmin=None, xmax=None)
+    sp.specfit.EQW(plot=True, plotcolor='g', fitted=False, 
+                   components=False, annotate=True, 
+                   loc='lower left', xmin=None, xmax=None)
 
     sp2 = sp.copy()
-    EQWs = []              
+    EQWs = np.zeros(n)
 
     for w in range(n):
         sp2.data = sp.data + np.random.randn(sp.data.size)*sp.error
-        sp2.baseline(xmin=xmin, xmax=xmax, exclude=[exclude_min,exclude_max], 
+        sp2.baseline(xmin=xmin, xmax=xmax, 
+                     exclude=[exclude_min,exclude_max],  
                      subtract=False, highlight_fitregion=False, 
-                     selectregion=True, order=0)           
+                     selectregion=True, order=0)
         sp2.specfit(fittype='voigt', guesses=sp.specfit.parinfo.values)
-        dist = sp2.specfit.EQW(plotcolor='g', fitted=False, components=False, 
-                               annotate=True, loc='lower left', xmin=None, xmax=None)
+        dist = sp2.specfit.EQW(plotcolor='g', fitted=False, 
+                               components=False, annotate=True,  
+                               loc='lower left', xmin=None, xmax=None)
 
-        EQWs.append(dist)
-    EQWs = np.array(EQWs)
-    EQWs = EQWs*10000
+        # Convert from microns to angstroms
+        EQWs[w] = dist*10000.0
 
     plt.figure()
     mu,sigma = norm.fit(EQWs) 
     print mu, sigma
 
-    n,bins,patches = plt.hist(EQWs, 10, normed=True, facecolor='green', histtype='stepfilled')      
+    num_bins = np.floor(np.log10(n)) * 10
+
+    # Light blue histogram for contrast and for R/G colorblind folks
+    n,bins,patches = plt.hist(EQWs, num_bins, normed=True, 
+                              facecolor='lightblue',  
+                              histtype='stepfilled')      
     y = mlab.normpdf(bins,mu,sigma)
     plt.plot(bins,y,'r--',linewidth=2)
-    plt.grid(True)
+
+    # Plot the median and 68th percentile values for comparison
+    perc = np.percentile(EQWs, [16, 50, 84])
+    ax = plt.gca()
+    for p_value in perc:
+        ax.axvline(p_value,linestyle=":",color="k",lw=1.5)
+
     plt.ylabel('Probability')
     plt.xlabel('EQW')           
     plt.show()
+
+if __name__=="__main__":
+    # xmin,xmax,exclude_min,exclude_max,n
+    equivalent_width('2m1821_61_08jun11.txt',1.250, 1.255, 1.2514, 
+                     1.2538, 100)
